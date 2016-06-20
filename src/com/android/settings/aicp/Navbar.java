a13e8f8bed2a012e6e29c920edebc92bb2889faf
deadman96385 <seanhoyt963@gmail.com>
2016-06-20 02:46:20 -0500

    Remove slim seekbar import
    It is not used and we do not currently use it

--- src/com/android/settings/aicp/Navbar.java
+++ src/com/android/settings/aicp/Navbar.java
@@ -44,7 +44,6 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SlimSeekBarPreference;
 import android.preference.SwitchPreference;
 
 import com.android.internal.utils.du.ActionConstants;
